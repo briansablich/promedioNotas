fc6da9c48846e67ca11d614458d567546fad98ad
Brian_Sablich <sablichbrian@yahoo.com.ar>
2024-12-26 13:57:33 -0300

    first commit

---
+++ src/main/java/com/promedionotas/promedionotas/PromedionotasApplication.java
@@ -0,0 +1,13 @@
+package com.promedionotas.promedionotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromedionotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromedionotasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/promedionotas/promedionotas/PromedionotasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.promedionotas.promedionotas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromedionotasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
